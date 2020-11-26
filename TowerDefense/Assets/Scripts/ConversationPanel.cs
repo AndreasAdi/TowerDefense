0201c1c3ead8611fece395c7d0f51c4ca00661a0
EnricoPucci01 <49854430+EnricoPucci01@users.noreply.github.com>
2020-11-26 12:32:09 +0700

    tambah sound scene 1
    Tambah Sound Gameplay,main menu,dialog untuk scene 1

--- TowerDefense/Assets/Scripts/ConversationPanel.cs
+++ TowerDefense/Assets/Scripts/ConversationPanel.cs
@@ -7,8 +7,11 @@ public class ConversationPanel : MonoBehaviour
     // Start is called before the first frame update
     int indexPage=0;
     List<Transform> conversationPanel= new List<Transform>();
+    GameObject camera;
     void Start()
     {
+        camera = GameObject.Find("Camera");
+        camera.GetComponent<SoundEffect>().playSound(0, true, 1);
         Time.timeScale = 0;
         Debug.Log("child Count "+transform.childCount);
         //transform.GetChild(0).gameObject.SetActive(false);
@@ -21,9 +24,14 @@ public class ConversationPanel : MonoBehaviour
     public void nextPage() {
         conversationPanel[indexPage].gameObject.SetActive(false);
         indexPage++;
+        if (indexPage == 1) {
+            camera.GetComponent<SoundEffect>().playSound(1, true, 1);
+        }
+        
         if (indexPage >= transform.childCount) {
             //startGame()
             Time.timeScale = 1;
+            camera.GetComponent<SoundEffect>().playSound(2, true, 1);
         }
     }
 }
---
+++ TowerDefense/Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioClip[] soundEff; 
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Awake()
+    {   audio = gameObject.AddComponent(typeof(AudioSource)) as AudioSource; }
+
+    public void playSound(int i, bool loop,float volume)
+    {   audio.Stop();
+        if (loop == true)
+        {
+            audio.volume = volume;
+            audio.clip = soundEff[i];
+            audio.loop = true;
+            audio.Play();
+        }
+        else
+        { audio.PlayOneShot(soundEff[i]); }
+    }
+    public void stopSound()
+    {
+        audio.Stop();
+    }
+}
