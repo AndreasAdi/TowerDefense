231fe0f724d13a2aad8069d17f0920b15a35337d
Andreas Adi <49746748+AndreasAdi@users.noreply.github.com>
2020-11-30 16:09:59 +0700

    perbaikan canvas

--- TowerDefense/Assets/Scripts/CanvasSetting.cs
+++ TowerDefense/Assets/Scripts/CanvasSetting.cs
@@ -16,7 +16,7 @@ public class CanvasSetting : MonoBehaviour
     void Start()
     {
         koin = 20;
-        Koin_txt.text = "Koin : " + koin;
+        Koin_txt.text = "Coin : " + koin;
 
        
         //misi =  GameObject.Find("Canvas").GetComponent<SpawnMusuh>().jumlahMusuh;
@@ -27,13 +27,13 @@ public class CanvasSetting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Koin_txt.text = "Koin : " + koin;
+        Koin_txt.text = "Coin : " + koin;
         //misi_txt.text = "Golem:"+(musuh-misi)+"/10";
     }
 
     public void tambah_koin()
     {
         koin+=10;
-        Koin_txt.text = "Koin : " + koin;
+        Koin_txt.text = "Coin : " + koin;
     }
 }
